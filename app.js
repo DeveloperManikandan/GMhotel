f4769fad23819d69030e94b56e225a1f06f321e1
Manikandan G. <manisaragan@gmail.com>
2024-08-31 20:39:03 +0530

    first commit

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ app.use(cors());
 const dbconfig = require("./db");
 const roomsRoute = require('./routes/roomsRoute')
 const userRoute = require('./routes/userRoute')
-const bookingRoute = require('./routes/bookingRoute')
+const bookingRoute = require('./routes/bookingRoute');
 app.use(express.json());
 
 app.use('/api/rooms',roomsRoute);
 app.use('/api/user',userRoute);
 app.use('/api/bookings',bookingRoute);
 
-const port = process.env.PORT || 5000;
-
-app.listen(port,console.log(`hiii`));
+const port = 5000;
+app.listen(port,console.log(`server running`));
